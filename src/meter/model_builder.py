52621f7f12a291367f407a34a3cdee675a83ed9c
Marcus Pereira <mav.pereira@outlook.com>
2025-02-23 19:42:22 -0600

    add method to split dataset

--- src/meter/model_builder.py
+++ src/meter/model_builder.py
@@ -1,14 +1,31 @@
 from sklearn.tree import DecisionTreeClassifier
 import numpy as np
 from sklearn.metrics import accuracy_score
-
+from numpy.typing import NDArray
 # Importing the parent: DataPreprocessing class from data_preprocess.py
 from meter.data_preprocess import DataPreprocessing 
 
 
 class ModelBuilder(DataPreprocessing):
+
+
     def __init__(self, *args, **kwargs):
         super(ModelBuilder, self).__init__(*args, **kwargs)
+
+
+    def split_dataset(self, X, y):
+        from sklearn.model_selection import train_test_split
+        X_train_validation, X_test, y_train_validation, y_test = train_test_split(X,y, test_size = 0.2, random_state=12)
+
+        X_train, X_validation, y_train, y_validation = train_test_split(X_train_validation,y_train_validation, test_size = 0.2, random_state=99)
+
+        # Sanity check
+        assert X_test.shape[0] == y_test.shape[0]
+        assert X_train.shape[0] == y_train.shape[0]
+        assert X_validation.shape[0] == y_validation.shape[0]
+
+        return (X_train, X_test, X_validation), (y_train, y_test, y_validation)
+
 
     def dt(self, X_train, X_test, y_train, y_test):
         #Create DT model
