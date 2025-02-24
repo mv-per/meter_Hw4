bd5804284bc2609944561f23ad5dcd62ef21e0f2
Marcus Pereira <mav.pereira@outlook.com>
2025-02-23 19:37:17 -0600

    add method to remove labels

--- src/meter/data_preprocess.py
+++ src/meter/data_preprocess.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pandas as pd
 
 
@@ -5,14 +6,20 @@ class DataPreprocessing():
     def __init__(self):
         pass
 
-    def load_data(self, path):
+    def load_data(self, path, y_column:int|None=None):
         data = pd.read_csv(path, sep="\t", header=None).dropna()
-
+        y = None
         print(data.head())
 
         data = data.to_numpy()
 
-        return self.normalize_data(data)
+        if y_column is not None:
+            y = data[:,y_column]
+            data=  np.delete(data, [y_column], axis=1)
+
+        data= self.normalize_data(data)
+
+        return data, y
     
     def normalize_data(self, data):
         from sklearn.preprocessing import StandardScaler
