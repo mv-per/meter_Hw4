d2587ac93c546d4820a1f9ea45c06492ccd7010d
Marcus Pereira <mav.pereira@outlook.com>
2025-02-23 20:05:32 -0600

    run pre-commit on repository

--- src/meter/data_preprocess.py
+++ src/meter/data_preprocess.py
@@ -2,11 +2,11 @@ import numpy as np
 import pandas as pd
 
 
-class DataPreprocessing():
+class DataPreprocessing:
     def __init__(self):
         pass
 
-    def load_data(self, path, y_column:int|None=None):
+    def load_data(self, path, y_column: int | None = None):
         data = pd.read_csv(path, sep="\t", header=None).dropna()
         y = None
         print(data.head())
@@ -14,13 +14,13 @@ class DataPreprocessing():
         data = data.to_numpy()
 
         if y_column is not None:
-            y = data[:,y_column]
-            data=  np.delete(data, [y_column], axis=1)
+            y = data[:, y_column]
+            data = np.delete(data, [y_column], axis=1)
 
-        data= self.normalize_data(data)
+        data = self.normalize_data(data)
 
         return data, y
-    
+
     def normalize_data(self, data):
         from sklearn.preprocessing import StandardScaler
 
--- src/meter/model_builder.py
+++ src/meter/model_builder.py
@@ -2,15 +2,17 @@ from sklearn.tree import DecisionTreeClassifier
 import numpy as np
 
 
-
-class ModelBuilder():
-
-
+class ModelBuilder:
     def split_dataset(self, X, y):
         from sklearn.model_selection import train_test_split
-        X_train_validation, X_test, y_train_validation, y_test = train_test_split(X,y, test_size = 0.2, random_state=12)
 
-        X_train, X_validation, y_train, y_validation = train_test_split(X_train_validation,y_train_validation, test_size = 0.2, random_state=99)
+        X_train_validation, X_test, y_train_validation, y_test = train_test_split(
+            X, y, test_size=0.2, random_state=12
+        )
+
+        X_train, X_validation, y_train, y_validation = train_test_split(
+            X_train_validation, y_train_validation, test_size=0.2, random_state=99
+        )
 
         # Sanity check
         assert X_test.shape[0] == y_test.shape[0]
@@ -19,24 +21,23 @@ class ModelBuilder():
 
         return (X_train, X_test, X_validation), (y_train, y_test, y_validation)
 
-
-    def _get_accuracy(self,ytrue, y_predicted) -> None:
+    def _get_accuracy(self, ytrue, y_predicted) -> None:
         from sklearn.metrics import accuracy_score
+
         self.accuracy = accuracy_score(ytrue, y_predicted)
 
-    def fit_classifier(self,X_train, y_train ) -> DecisionTreeClassifier:
+    def fit_classifier(self, X_train, y_train) -> DecisionTreeClassifier:
         classifier = DecisionTreeClassifier()
         classifier.fit(X_train, y_train)
         return classifier
 
     def classify(self, X_train, X_test, y_train, y_test):
-        #Create DT model
-        
+        # Create DT model
 
-        #Train the model
-        classifier= self.fit_classifier(X_train, y_train)
+        # Train the model
+        classifier = self.fit_classifier(X_train, y_train)
 
-        #Test the model
+        # Test the model
         y_predicted = classifier.predict(X_test)
 
         error = 0
@@ -45,7 +46,7 @@ class ModelBuilder():
 
         # total_accuracy = 1 - error / len(y_test)
 
-        #get performance
+        # get performance
         self._get_accuracy(y_test, y_predicted)
 
         return classifier
